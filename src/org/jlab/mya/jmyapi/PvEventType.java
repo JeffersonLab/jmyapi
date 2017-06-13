1439a0ee1704b7831da1446b000cdda2f6012f97
slominskir <Ryan.Slominski@gmail.com>
2017-06-13 11:43:39 -0400

    Updated event type enum

--- src/org/jlab/mya/jmyapi/PvEventType.java
+++ src/org/jlab/mya/jmyapi/PvEventType.java
@@ -5,5 +5,9 @@ package org.jlab.mya.jmyapi;
  * @author ryans
  */
 public enum PvEventType {
-    DISCONTINUITY, SCALAR, VECTOR
+    UPDATE, // 0
+    ARCHIVER_SHUTDOWN, // 1
+    ARCHIVING_OF_CHANNEL_TURNED_OFF, // 2
+    NETWORK_DISCONNECTION, // 3
+    UNKNOWN_UNAVAILABILTY // 4
 }
