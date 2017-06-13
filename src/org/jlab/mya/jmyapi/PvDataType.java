e951c9db828363abc66092a1f41542ec2688039b
slominskir <Ryan.Slominski@gmail.com>
2017-06-13 11:29:00 -0400

    Updated data type enum

--- src/org/jlab/mya/jmyapi/PvDataType.java
+++ src/org/jlab/mya/jmyapi/PvDataType.java
@@ -5,5 +5,11 @@ package org.jlab.mya.jmyapi;
  * @author ryans
  */
 public enum PvDataType {
-    zero, one, two, three, four, five, DBR_DOUBLE, seven;
+    DBR_STRING, // 0
+    DBR_SHORT, // 1
+    DBR_FLOAT, // 2
+    DBR_ENUM, // 3
+    DBR_CHAR, // 4
+    DBR_LONG, // 5
+    DBR_DOUBLE, // 6
 }
