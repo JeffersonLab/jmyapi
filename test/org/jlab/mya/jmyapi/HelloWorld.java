0a292d09b5bacf3c1684639f85c809bdd2d61dda
slominskir <Ryan.Slominski@gmail.com>
2017-06-13 16:48:20 -0400

    Use Generic Type to also determine if array or scalar not just datatype

--- test/org/jlab/mya/jmyapi/HelloWorld.java
+++ test/org/jlab/mya/jmyapi/HelloWorld.java
@@ -20,19 +20,20 @@ public class HelloWorld {
      * @throws java.io.IOException
      */
     public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
-        
+
         //System.out.println(MyaUtil.fromMyaTimestamp(6276360291443298313L).atZone(ZoneId.systemDefault()));
-        
         ArchiverQueryService service = new ArchiverQueryService(ArchiverDeployment.ops);
 
         //String pv = "DCPHP2ADC10";
         String pv = "IPM3F09.XPOS";
-        Instant begin = LocalDateTime.parse("2016-09-22T08:43:00").atZone(ZoneId.systemDefault()).toInstant();
-        Instant end = LocalDateTime.parse("2017-04-22T08:43:28").atZone(ZoneId.systemDefault()).toInstant();
-        
-        List<PvRecord<Float[]>> recordList = service.find(pv, Float[].class, begin, end);
+        Instant begin
+                = LocalDateTime.parse("2016-09-22T08:43:00").atZone(ZoneId.systemDefault()).toInstant();
+        Instant end
+                = LocalDateTime.parse("2017-04-22T08:43:28").atZone(ZoneId.systemDefault()).toInstant();
 
-        for (PvRecord<Float[]> record : recordList) {
+        List<PvRecord<Float>> recordList = service.find(pv, Float.class, begin, end);
+
+        for (PvRecord<Float> record : recordList) {
             System.out.println(record.toColumnString());
         }
     }
