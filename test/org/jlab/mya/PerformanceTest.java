8db420a4a1fb94f3347c91ffcc7a3fac1c4bc673
Adam Carpenter <adamc@jlab.org>
2019-04-04 10:52:25 -0400

    Change deployment from opsfb to history

--- test/org/jlab/mya/PerformanceTest.java
+++ test/org/jlab/mya/PerformanceTest.java
@@ -20,7 +20,7 @@ public class PerformanceTest {
 
     @Test
     public void doTest() throws SQLException, IOException {
-        DataNexus nexus = new OnDemandNexus(Deployment.opsfb);
+        DataNexus nexus = new OnDemandNexus(Deployment.history);
         IntervalService service = new IntervalService(nexus);
 
         String pv = "IDC1G03sh_cur";
