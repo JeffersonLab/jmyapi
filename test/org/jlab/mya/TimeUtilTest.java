c1d10eb2020de64c686b9ba597202bda48787184
Adam Carpenter <adamc@jlab.org>
2019-04-24 16:41:22 -0400

    Rolled back to match "old" MYA timestamp format.
    Left "new" MYA timestamp format expected results, etc. commented out

--- test/org/jlab/mya/TimeUtilTest.java
+++ test/org/jlab/mya/TimeUtilTest.java
@@ -51,9 +51,10 @@ public class TimeUtilTest {
 
         Instant instant = LocalDateTime.parse(dateStr).atZone(
                 ZoneId.systemDefault()).toInstant();
-        long expResult = 398156031588037989L;
+        long expResult = 6370496505408607835L;
+//        long expResult = 398156031588037989L;
         long result = TimeUtil.toMyaTimestamp(instant);
-        assertEquals(expResult, result);
+        assertEquals(expResult, result, 4);
         
     }
 
@@ -65,7 +66,8 @@ public class TimeUtilTest {
         System.out.println("fromMyaTimestamp");
         String dateStr = "2017-01-01T00:00:01.749013325";
 
-        long timestamp = 398156031588037989L;
+        long timestamp = 6370496505408607835L;
+//        long timestamp = 398156031588037989L;
         Instant expResult = LocalDateTime.parse(dateStr).atZone(
                 ZoneId.systemDefault()).toInstant();
         Instant result = TimeUtil.fromMyaTimestamp(timestamp);
