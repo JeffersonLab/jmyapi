7ce075dcfa14360b35f848379aa1c78a92e43cc8
Adam Carpenter <adamc@jlab.org>
2019-04-24 17:41:57 -0400

    Updated sampler method names and updated test case to match GraphicalSampler bug fix.

--- test/org/jlab/mya/service/SamplingServiceTest.java
+++ test/org/jlab/mya/service/SamplingServiceTest.java
@@ -10,11 +10,7 @@ import org.jlab.mya.DataNexus;
 import org.jlab.mya.Metadata;
 import org.jlab.mya.event.FloatEvent;
 import org.jlab.mya.nexus.OnDemandNexus;
-import org.jlab.mya.params.GraphicalSamplerParams;
-import org.jlab.mya.params.BasicSamplerParams;
-import org.jlab.mya.params.ImprovedSamplerParams;
-import org.jlab.mya.params.IntervalQueryParams;
-import org.jlab.mya.params.NaiveSamplerParams;
+import org.jlab.mya.params.*;
 import org.jlab.mya.stream.FloatEventStream;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -60,7 +56,7 @@ public class SamplingServiceTest {
      * Compare with "myget -l 24 -c R123PMES -b 2017-01-01 -e 2017-01-25 -f 6"
      */
     @Test
-    public void testNaiveSampler() throws Exception {
+    public void testBinnedSampler() throws Exception {
         
         String pv = "R123PMES";
         Instant begin = LocalDateTime.parse("2017-01-01T00:00:00").atZone(
@@ -71,12 +67,12 @@ public class SamplingServiceTest {
         int fractionalDigits = 6; // microseconds; seems to be max precision of myget
 
         Metadata metadata = sampleService.findMetadata(pv);
-        NaiveSamplerParams params = new NaiveSamplerParams(metadata, begin,
-                end, limit);            
-        
+        BinnedSamplerParams params = new BinnedSamplerParams(metadata, begin,
+                end, limit);
+
         long expSize = 24; // We limit to 24, but we know historical data only has 21
         List<FloatEvent> eventList = new ArrayList<>();
-        try (FloatEventStream stream = sampleService.openNaiveSamplerFloatStream(params)) {
+        try (FloatEventStream stream = sampleService.openBinnedSamplerFloatStream(params)) {
             FloatEvent event;
             while ((event = stream.read()) != null) {
                 eventList.add(event);
@@ -123,7 +119,7 @@ public class SamplingServiceTest {
      * Test improved sampler.
      */
     @Test
-    public void testImprovedSampler() throws Exception {
+    public void testEventSampler() throws Exception {
         
         String pv = "R123PMES";
         // 20 second test
@@ -148,12 +144,12 @@ public class SamplingServiceTest {
         
         //System.out.println("count: " + count);
         
-        ImprovedSamplerParams samplerParams = new ImprovedSamplerParams(metadata, begin,
+        EventSamplerParams samplerParams = new EventSamplerParams(metadata, begin,
                 end, limit, count);            
         
         long expSize = 10; // Not sure it will always be exact, might be +/- 1 in some combinations of count and limit
         List<FloatEvent> eventList = new ArrayList<>();
-        try (FloatEventStream stream = sampleService.openImprovedSamplerFloatStream(samplerParams)) {
+        try (FloatEventStream stream = sampleService.openEventSamplerFloatStream(samplerParams)) {
             FloatEvent event;
             while ((event = stream.read()) != null) {
                 eventList.add(event);
@@ -169,14 +165,14 @@ public class SamplingServiceTest {
      * Test advanced sampler.
      */
     @Test
-    public void testAdvancedSampler() throws Exception {
+    public void testGraphicalSampler() throws Exception {
 
         // Limited test        
         String pv = "R12XGMES";
         Instant begin = LocalDateTime.parse("2017-02-11T00:00:00").atZone(
                 ZoneId.systemDefault()).toInstant();
         Instant end = LocalDateTime.parse("2017-02-11T02:30:00").atZone(
-                ZoneId.systemDefault()).toInstant();        
+                ZoneId.systemDefault()).toInstant();
 
 //        String pv = "R123PMES";
 //        Instant begin = LocalDateTime.parse("2016-01-01T00:00:00").atZone(
@@ -194,8 +190,11 @@ public class SamplingServiceTest {
                 end, numBins, count);
 
         // Impossible to know how many data points will be generated a priori since every disconnect will be represented.
-        // Beyond that, it should be min(data points, start point + end point + number of bins)
-        long expSize = 12; // Since we know ahead of time there are 20 points of data, start + end + 10 bin points + zero non-update events = 12
+        // The expected size gets complicated to predict.  Number of actual bins is a complicated calculation based on determining
+        // the smallest bin size that produces no more than numBins-2.  Then each bin can produce min, max, and largest triangle three bucket (lttb) point
+        // and also return an unlimited number of non-update events plus surrounding update events.  A good rule of thumb for number of points returned is
+        // between 1*(numBins-2) + numNonUpdateEvents*3 + 2 and 3*(numBins-2) + numNonUpdateEvents*3 + 2
+        long expSize = 15; // 2 + 1*8 = 10, 2 + 3*8 = 26, so it's a broad range
 
         List<FloatEvent> eventList = new ArrayList<>();
         try (FloatEventStream stream = sampleService.openGraphicalSamplerFloatStream(samplerParams)) {
@@ -207,6 +206,8 @@ public class SamplingServiceTest {
             System.out.println("Downsampled num: " + eventList.size());
             System.out.println("Max Exepected update num: " + ( (numBins-2)*3 + 2) );
         }
+
+        // Since we know ahead of time there are 20 points of data, start + end + (10 bins with min,max,lttb points) + zero non-update events = between 12 and 20
         assertEquals("List size does not match expected", expSize, eventList.size());
     }    
 }
