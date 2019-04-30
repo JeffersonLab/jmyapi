e5476eea3a93d8633d0e91c5ae00468befc055b4
Adam Carpenter <adamc@jlab.org>
2019-04-30 13:42:49 -0400

    Updated to move point query to happen before interval query stream.  Fixed occasional deadlock issue.

--- src/org/jlab/mya/service/AnalysisService.java
+++ src/org/jlab/mya/service/AnalysisService.java
@@ -37,23 +37,23 @@ public class AnalysisService extends IntervalService {
         RunningStatistics rs = new RunningStatistics();
         PointQueryParams pqParams = new PointQueryParams(params.getMetadata(), params.getBegin());
         PointService pqService = new PointService(this.nexus);
+
+        // Get the first event from before the interval, then map it to an event that matches the start of the query.  This will
+        // make the duration, and other statistics correct for the query.  If null, then there was no point in the archiver from before
+        // the interval query and the interval query should catch the first point if it existed inside the interval
+        FloatEvent event = pqService.findFloatEvent(pqParams);
+        if ( event != null ) {
+            rs.push(new FloatEvent(TimeUtil.toMyaTimestamp(params.getBegin()), event.getCode(), event.getValue()));
+        }
+
         try ( FloatEventStream stream = this.openFloatStream(params) ) {
-            
-            // Get the first event from before the interval, then map it to an event that matches the start of the query.  This will
-            // make the duration, and other statistics correct for the query.  If null, then there was no point in the archiver from before
-            // the interval query and the interval query should catch the first point if it existed inside the interval
-            FloatEvent event = pqService.findFloatEvent(pqParams);
-            if ( event != null ) {
-                rs.push(new FloatEvent(TimeUtil.toMyaTimestamp(params.getBegin()), event.getCode(), event.getValue()));
-            }
-            
             FloatEvent last = event;
             while ( (event  = stream.read()) != null ) {
                 last = event;
                 // Add each event - the push method updates the statistics based on the new data
                 rs.push(event);
             }
-            
+
             // Create an event that represents the value of the signal at the end of the query
             if ( last != null ) {
                 rs.push(new FloatEvent(TimeUtil.toMyaTimestamp(params.getEnd()), last.getCode(), last.getValue()));
