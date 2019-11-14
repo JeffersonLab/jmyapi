9c45f31681e1ae5929329798f363749ba81a6af1
ryans <ryans@jlab.org>
2019-11-14 14:30:18 -0500

    Updated performance test to show more information and compare different queries and streams

--- src/test/java/org/jlab/mya/PerformanceTest.java
+++ src/test/java/org/jlab/mya/PerformanceTest.java
@@ -1,9 +1,13 @@
 package org.jlab.mya;
 
+import org.jlab.mya.analysis.RunningStatistics;
 import org.jlab.mya.event.FloatEvent;
 import org.jlab.mya.nexus.OnDemandNexus;
+import org.jlab.mya.params.GraphicalEventBinSamplerParams;
 import org.jlab.mya.params.IntervalQueryParams;
+import org.jlab.mya.params.PointQueryParams;
 import org.jlab.mya.service.IntervalService;
+import org.jlab.mya.service.PointService;
 import org.jlab.mya.stream.FloatEventStream;
 
 import java.io.IOException;
@@ -12,6 +16,9 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 
+import org.jlab.mya.stream.wrapped.BoundaryAwareStream;
+import org.jlab.mya.stream.wrapped.FloatAnalysisStream;
+import org.jlab.mya.stream.wrapped.FloatGraphicalEventBinSampleStream;
 import org.junit.Test;
 
 /**
@@ -24,6 +31,7 @@ public class PerformanceTest {
     public void doTest() throws SQLException, IOException {
         DataNexus nexus = new OnDemandNexus("history");
         IntervalService service = new IntervalService(nexus);
+        PointService pointService = new PointService(nexus);
 
         String pv = "IDC1G03sh_cur";
         Instant begin
@@ -31,23 +39,41 @@ public class PerformanceTest {
         Instant end
                 = LocalDateTime.parse("2017-07-01T00:00:00").atZone(ZoneId.systemDefault()).toInstant();
 
+        Runtime rt = Runtime.getRuntime();
+        long startBytes;
+        long stopBytes;
+        long startMillis;
+        long stopMillis;
+
+        System.out.println("---- Metadata Query ----");
+        rt.gc();
+        startBytes = rt.totalMemory() - rt.freeMemory();
+        startMillis = System.currentTimeMillis();
+
         Metadata metadata = service.findMetadata(pv);
+
+        stopMillis = System.currentTimeMillis();
+        stopBytes = rt.totalMemory() - rt.freeMemory();
+        System.out.println("Elapsed (seconds): " + (stopMillis - startMillis) / 1000.0);
+        System.out.println("Memory used at this instant (MB): " + String.format("%,.2f", (stopBytes) / 1024.0 / 1024.0));
 
         IntervalQueryParams params = new IntervalQueryParams(metadata, begin, end);
 
-        Runtime rt = Runtime.getRuntime();
-        long memoryUsedBytes;
-        long start;
-        long stop;
-
-        memoryUsedBytes = rt.totalMemory() - rt.freeMemory();
-        System.out.println("memory used MB: " + (memoryUsedBytes / 1024.0 / 1024.0));
-
-        start = System.currentTimeMillis();
+        System.out.println("---- Event Count Query ----");
+        rt.gc();
+        startBytes = rt.totalMemory() - rt.freeMemory();
+        startMillis = System.currentTimeMillis();
         long count = service.count(params);
-        stop = System.currentTimeMillis();
-        System.out.println("count: " + count + "; took seconds: " + (stop - start) / 1000.0);
-        start = System.currentTimeMillis();
+        stopMillis = System.currentTimeMillis();
+        stopBytes = rt.totalMemory() - rt.freeMemory();
+        System.out.println("Elapsed (seconds): " + (stopMillis - startMillis) / 1000.0);
+        System.out.println("Memory used at this instant (MB): " + String.format("%,.2f", (stopBytes) / 1024.0 / 1024.0));
+        System.out.println("Event count: " + String.format("%,d", count));
+
+        System.out.println("---- Event Interval Query ----");
+        rt.gc();
+        startBytes = rt.totalMemory() - rt.freeMemory();
+        startMillis = System.currentTimeMillis();
         try (FloatEventStream stream = service.openFloatStream(params)) {
 
             FloatEvent event;
@@ -56,10 +82,90 @@ public class PerformanceTest {
                 //System.out.println(event);
             }
         }
-        stop = System.currentTimeMillis();
-        System.out.println("Stream Traversed seconds: " + (stop - start) / 1000.0);
+        stopMillis = System.currentTimeMillis();
+        stopBytes = rt.totalMemory() - rt.freeMemory();
+        System.out.println("Elapsed (seconds): " + (stopMillis - startMillis) / 1000.0);
+        System.out.println("Memory used at this instant (MB): " + String.format("%,.2f", (stopBytes) / 1024.0 / 1024.0));
 
-        memoryUsedBytes = rt.totalMemory() - rt.freeMemory();
-        System.out.println("memory used MB: " + (memoryUsedBytes / 1024.0 / 1024.0));
+        System.out.println("---- Prior Point Lookup ----");
+        rt.gc();
+        startBytes = rt.totalMemory() - rt.freeMemory();
+        startMillis = System.currentTimeMillis();
+        PointQueryParams pointParams = new PointQueryParams(metadata, begin);
+        FloatEvent priorPoint = pointService.findFloatEvent(pointParams);
+        stopMillis = System.currentTimeMillis();
+        stopBytes = rt.totalMemory() - rt.freeMemory();
+        System.out.println("Elapsed (seconds): " + (stopMillis - startMillis) / 1000.0);
+        System.out.println("Memory used at this instant (MB): " + String.format("%,.2f", (stopBytes) / 1024.0 / 1024.0));
+
+        System.out.println("---- BoundaryAwareStream ----");
+        rt.gc();
+        startBytes = rt.totalMemory() - rt.freeMemory();
+        startMillis = System.currentTimeMillis();
+
+        try (
+                final FloatEventStream stream = service.openFloatStream(params);
+                final BoundaryAwareStream<FloatEvent> boundaryStream = new BoundaryAwareStream<>(stream, begin, end, priorPoint, false);
+        ) {
+
+            FloatEvent event;
+
+            while ((event = boundaryStream.read()) != null) {
+                //System.out.println(event);
+            }
+        }
+
+        stopMillis = System.currentTimeMillis();
+        stopBytes = rt.totalMemory() - rt.freeMemory();
+        System.out.println("Elapsed (seconds): " + (stopMillis - startMillis) / 1000.0);
+        System.out.println("Memory used at this instant (MB): " + String.format("%,.2f", (stopBytes) / 1024.0 / 1024.0));
+
+
+
+
+        System.out.println("---- FloatAnalysisStream ----");
+        rt.gc();
+        startBytes = rt.totalMemory() - rt.freeMemory();
+        startMillis = System.currentTimeMillis();
+
+        try (
+                final FloatEventStream stream = service.openFloatStream(params);
+                final FloatAnalysisStream analysisStream = new FloatAnalysisStream(stream, new short[]{RunningStatistics.INTEGRATION});
+        ) {
+
+            FloatEvent event;
+
+            while ((event = analysisStream.read()) != null) {
+                //System.out.println(event);
+            }
+        }
+
+        stopMillis = System.currentTimeMillis();
+        stopBytes = rt.totalMemory() - rt.freeMemory();
+        System.out.println("Elapsed (seconds): " + (stopMillis - startMillis) / 1000.0);
+        System.out.println("Memory used at this instant (MB): " + String.format("%,.2f", (stopBytes) / 1024.0 / 1024.0));
+
+
+        System.out.println("---- GraphicalSamplerStream ----");
+        rt.gc();
+        startBytes = rt.totalMemory() - rt.freeMemory();
+        startMillis = System.currentTimeMillis();
+
+        try (
+                final FloatEventStream stream = service.openFloatStream(params);
+                final FloatGraphicalEventBinSampleStream samplerStream = new FloatGraphicalEventBinSampleStream(stream, new GraphicalEventBinSamplerParams(3, count));
+        ) {
+
+            FloatEvent event;
+
+            while ((event = samplerStream.read()) != null) {
+                //System.out.println(event);
+            }
+        }
+
+        stopMillis = System.currentTimeMillis();
+        stopBytes = rt.totalMemory() - rt.freeMemory();
+        System.out.println("Elapsed (seconds): " + (stopMillis - startMillis) / 1000.0);
+        System.out.println("Memory used at this instant (MB): " + String.format("%,.2f", (stopBytes) / 1024.0 / 1024.0));
     }
 }
