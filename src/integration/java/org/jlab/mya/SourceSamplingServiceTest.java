c081367a9de71a7b0348ed1b67090f2052026141
ryans <ryans@jlab.org>
2022-10-24 09:58:26 -0400

    Integration tests Updated

--- src/integration/java/org/jlab/mya/SourceSamplingServiceTest.java
+++ src/integration/java/org/jlab/mya/SourceSamplingServiceTest.java
@@ -7,17 +7,11 @@ import java.util.List;
 import org.jlab.mya.nexus.DataNexus;
 import org.jlab.mya.nexus.OnDemandNexus;
 import org.jlab.mya.stream.EventStream;
-import org.jlab.mya.Metadata;
-import org.jlab.mya.TimeUtil;
 import org.jlab.mya.event.FloatEvent;
-
-import static org.junit.Assert.assertEquals;
 
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-
-import static org.junit.Assert.fail;
 
 /**
  * Unit tests for SourceSamplingService.  Sampling done in-database.   See also: stream.wrapped.ApplicationLevelSamplingTest.
@@ -30,20 +24,20 @@ public class SourceSamplingServiceTest {
 
     @Before
     public void setUp() {
-        nexus = new OnDemandNexus("history");
+        nexus = new OnDemandNexus("docker");
     }
 
     /**
      * Test naive sampler.
      * <p>
-     * Compare with "myget -l 24 -c R123PMES -b 2017-01-01 -e 2017-01-25 -f 6"
+     * Compare with "myget -l 24 -c IGL1I00POTcurrent -b 2019-08-12 -e 2019-08-13 -f 6"
      */
     @Test
     public void testMyGetSampler() throws Exception {
 
-        String pv = "R123PMES";
-        Instant begin = TimeUtil.toLocalDT("2017-01-01T00:00:00");
-        Instant end = TimeUtil.toLocalDT("2017-01-25T00:00:00");
+        String pv = "channel1";
+        Instant begin = TimeUtil.toLocalDT("2019-08-12T00:00:00");
+        Instant end = TimeUtil.toLocalDT("2019-08-13T00:00:00");
         long limit = 24;
         int fractionalDigits = 6; // microseconds; seems to be max precision of myget
 
@@ -55,25 +49,22 @@ public class SourceSamplingServiceTest {
             FloatEvent event;
             while ((event = stream.read()) != null) {
                 eventList.add(event);
-                //        System.out.println(event.toString(fractionalDigits));
+                //System.out.println(event.toString(fractionalDigits));
             }
         }
         Assert.assertEquals(expSize, eventList.size());
-        if (eventList.size() != expSize) {
-            Assert.fail("List size does not match expected");
-        }
     }
 
     /**
      * Test basic sampler.
      * <p>
-     * Compare with: "mySampler -b 2017-01-01 -s 1d -n 24 R123PMES"
+     * Compare with: "mySampler -b 2019-08-12 -s 1d -n 24 IGL1I00POTcurrent"
      */
     @Test
     public void testMySampler() throws Exception {
 
-        String pv = "R123PMES";
-        Instant begin = TimeUtil.toLocalDT("2017-01-01T00:00:00");
+        String pv = "channel1";
+        Instant begin = TimeUtil.toLocalDT("2019-08-12T00:00:00");
         long stepMilliseconds = 86400000;
         long sampleCount = 24;
 
